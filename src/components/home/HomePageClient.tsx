88cf337130d854c5bc1f38006740ee35a7f2cb49
mareckiw <48551365+mareckiw@users.noreply.github.com>
2026-07-07 12:35:46 +0200

    Update text color for accommodation details

--- src/components/home/HomePageClient.tsx
+++ src/components/home/HomePageClient.tsx
@@ -185,22 +185,22 @@ export default function HomePageClient({ calendarEvent }: { calendarEvent: Calen
                   <div className="border-t border-gray-200 dark:border-gray-700 pt-4">
                     <h3 className="font-bold text-lg text-[#568770]">Szczegóły noclegu</h3>
                 
-                    <p>Noclegi są w:</p>
-                    <ul className="list-disc list-inside ml-4 my-2">
+                    <p className="text-gray-800 dark:text-gray-100">Noclegi są w:</p>
+                    <ul className="list-disc list-inside ml-4 my-2 text-gray-800 dark:text-gray-100">
                       <li>
                         <a href="https://maps.app.goo.gl/edNAZ8fvNF9L2xSr5" target="_blank" rel="noopener noreferrer" className="text-rose-600 dark:text-rose-400 underline hover:no-underline">Willa Rufina</a>
                       </li>
                     </ul>
                 
-                    <p className="mb-2">Doba hotelowa trwa od 13:00 i kończy się o 11:00. Wcześniejsze zameldowanie możliwe jest w zależności od dostępności i jeśli macie takie życzenie to zadzwońcie to hotelu.</p>
+                    <p className="mb-2 text-gray-800 dark:text-gray-100">Doba hotelowa trwa od 13:00 i kończy się o 11:00. Wcześniejsze zameldowanie możliwe jest w zależności od dostępności i jeśli macie takie życzenie to zadzwońcie to hotelu.</p>
                 
-                    <p>Przypisaliśmy Wam pokoje i ich rozkład znajduje się:</p>
-                     <ul className="list-disc list-inside ml-4 my-2">
+                    <p className="text-gray-800 dark:text-gray-100">Przypisaliśmy Wam pokoje i ich rozkład znajduje się:</p>
+                     <ul className="list-disc list-inside ml-4 my-2 text-gray-800 dark:text-gray-100">
                       <li>
                         <a href="https://docs.google.com/spreadsheets/d/1jdrjo_YfHfTxFexC_AZtAUpZ-F0YvGzm3wko8NPxzkI/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="text-rose-600 dark:text-rose-400 underline hover:no-underline">tutaj</a>
                       </li>
                      </ul>
-                    <p>Śniadania są w godzinach 8-11 rano.</p>
+                    <p className="text-gray-800 dark:text-gray-100">Śniadania są w godzinach 8-11 rano.</p>
                   </div>
                 </div>
               </div>
